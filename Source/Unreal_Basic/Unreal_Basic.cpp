f6c766a52a40512381e70692d0dafcfe9dfdbde6
Cosmo <dlwogh1204@gmail.com>
2023-06-22 22:29:38 +0900

    Init Commit

---
+++ Source/Unreal_Basic/Unreal_Basic.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Unreal_Basic.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_Basic, "Unreal_Basic" );
---
+++ Source/Unreal_Basic/Unreal_BasicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Unreal_BasicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_BASIC_API AUnreal_BasicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
